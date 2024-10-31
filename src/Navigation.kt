f532aa40ce73431d2983b717df3c35575c2e7073
Ashipilov <Thesol590@yandex.ru>
2024-10-31 20:16:19 +0300

    3 commit

--- src/Navigation.kt
+++ src/Navigation.kt
@@ -1,18 +1,25 @@
 import calculator.Calculator
+import calculator.Operations
 
 class Navigation {
-    fun printList(list: List<String>){
-        for(i in list){
+    fun printList(list: List<String>) {
+        for (i in list) {
             println(i)
         }
     }
+
     fun startCalculator(calculator: Calculator) {
         while (true) {
             println(calculator.menu)
             val input = readlnOrNull()?.toIntOrNull()
             when (input) {
                 0 -> break
-                1 -> println("Выбрали сложение")
+                1 -> {
+                    println("Выбрали сложение")
+                    val answer = calculator.calculator(Operations.Addition)
+                    println(answer)
+                }
+
                 2 -> println("Выбрали вычитание")
                 3 -> println("Выбрали умножение")
                 4 -> println("Выбрали деление")
--- src/calculator/Calculator.kt
+++ src/calculator/Calculator.kt
@@ -1,10 +1,30 @@
 package calculator
 
 class Calculator {
+    val math = Math()
     val menu: String =
-            "0. Выход\n" +
-            "1. Сложение\n" +
-            "2. Вычетание\n" +
-            "3. Умножение\n" +
-            "4. Деление\n"
+        "0. Выход\n" +
+                "1. Сложение\n" +
+                "2. Вычетание\n" +
+                "3. Умножение\n" +
+                "4. Деление\n"
+
+    fun calculator(operation: Operations): Int {
+
+        val valOne = readln().toInt()
+        val valTwo = readln().toInt()
+
+
+
+        return when (operation) {
+            Operations.Addition -> {
+                math.add(valOne, valTwo)
+            }
+
+            Operations.Subtraction -> TODO()
+            Operations.Multiplication -> TODO()
+            Operations.Division -> TODO()
+        }
+
+    }
 }
