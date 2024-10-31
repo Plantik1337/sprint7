da580019d6e05e6c490897205344507e23da9e66
ArtemSh13 <mozg1303@ya.ru>
2024-10-31 19:35:19 +0300

    Add calculator navigation

--- src/Navigation.kt
+++ src/Navigation.kt
@@ -1,10 +1,25 @@
+import calculator.Calculator
+
 class Navigation {
     fun printList(list: List<String>){
         for(i in list){
             println(i)
         }
     }
-    fun startCalculator(){
+    fun startCalculator(calculator: Calculator) {
+        while (true) {
+            println(calculator.menu)
+            val input = readlnOrNull()?.toIntOrNull()
+            when (input) {
+                0 -> break
+                1 -> println("Выбрали сложение")
+                2 -> println("Выбрали вычитание")
+                3 -> println("Выбрали умножение")
+                4 -> println("Выбрали деление")
+                null -> println("Ошибка")
+                else -> println("Такого варианта нет")
 
+            }
+        }
     }
 }
--- src/calculator/Calculator.kt
+++ src/calculator/Calculator.kt
@@ -1,5 +1,10 @@
 package calculator
 
 class Calculator {
-
+    val menu: String =
+            "0. Выход\n" +
+            "1. Сложение\n" +
+            "2. Вычетание\n" +
+            "3. Умножение\n" +
+            "4. Деление\n"
 }
